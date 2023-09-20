4099381f18f589c4f8a59c57bec23b4a206d6d87
sabsvf <f.sabrina@gsuite.iff.edu.br>
2023-09-20 12:03:54 -0300

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# TP1
-Atividades do TP1
--- atividade.js
+++
@@ -1,3 +0,0 @@
-function TrocaImagem() {
-    document.getElementById("teste").src = "https://vovopalmirinha.com.br/wp-content/uploads/2016/05/bolo-chocolate-simples-1.jpg"
-}
